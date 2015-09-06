43f5fdf9dcd5f68d56792d40a7e0a246159577f4
Michael Kammeyer <me74kamm@gmail.com>
2015-09-06 11:50:51 -0500

    WIP - need to connect with cfg file properly and write to log and sol files

---
+++ exampleRandomSat/sat_random_search.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python2
+"""MAXSAT Random Search Algorithm"""
+
+__author__ = 'Aaron Scott Pope'
+__email__ = 'asphwb@mst.edu'
+
+#Insert code here... (You didn't think it'd be that easy, did you?)
--- randomSat/randomSat.py
+++ randomSat/randomSat.py
@@ -4,41 +4,67 @@ import re
 import sys
 import random
 import datetime
+import time
 
-f = open('1.cnf', 'r')
+if len(sys.argv) > 1:
+    config = open(sys.argv[1], 'r')
+else:
+    config = open('default.cfg', 'r')
 
-solution = []
+cnfFile = config.readline().split(': ')[1]
+seed = config.readline().split(': ')[1]
+evals = config.readline().split(': ')[1]
+runs = config.readline().split(': ')[1]
+logFile = config.readline().split(': ')[1]
+solFile = config.readline().split(': ')[1]
+
+f = open(cnfFile, 'r')
+log = open(logFile, 'w')
+sol = open(solFile, 'w')
 
 firstLine = f.readline()
 
 numVarsAndClauses = map(int, re.findall(r'\d+', firstLine))
 
-current_time = datetime.datetime.now().time()
-
+currentRun = 0
+fitness = 0
+current_time = time.mktime(datetime.datetime.now()).total_seconds
 random.seed(current_time)
 
-for i in range(0, numVarsAndClauses[0]):
-    solution.append(bool(random.getrandbits(1)))
+for run in range(1, runs):
+    currentEval = 0
+    while currnetEval <= evals:
+        currentEval+=1
 
-print solution
+        fitness = 0
+        f.seek(0)
+        for line in f:
+            words = line.split(' ')
 
-fitness = 0
-neg = False
+            if words[0] == 'p':           
+                numVarsAndClauses = map(int, re.findall(r'\d+', firstLine))
+                solution = []
+                for i in range(0, numVarsAndClauses[0]):
+                    solution.append(bool(random.getrandbits(1)))
+            
+            elif words[0] != 'c':
+                currentLine = False
 
-for line in f:
-    currentLine = True
-    words = line.split(' ')
-    for word in words:
-        num = int(word)
-        if num < 0:
-            num *= -1
-            currentLine = not solution[num-1]
-        elif num > 0:
-            currentLine = solution[num-1]
-        else:
-            if currentLine == True:
-                fitness+=1
+                for word in words:
+                    num = int(word)
+                    if num < 0:
+                        num *= -1
+                        currentLine = not solution[num-1]
+                    elif num > 0:
+                        currentLine = solution[num-1]
+                    
+                    if currentLine == True:
+                        fitness+=1
+                        break
 
-print fitness
 
-f.close()
+
+        print "Eval: ", currentEval
+        print "Fitness: ", fitness
+
+    f.close()
